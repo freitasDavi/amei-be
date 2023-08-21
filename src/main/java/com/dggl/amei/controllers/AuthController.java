cae68cad447b26a1966b0915c2a1b60ca98ad4af
Davi <davi.frrs@gmail.com>
2023-08-21 17:00:14 -0300

    feat: finishing auth

---
+++ src/main/java/com/dggl/amei/controllers/AuthController.java
@@ -0,0 +1,133 @@
+package com.dggl.amei.controllers;
+
+import com.dggl.amei.configuration.security.jwt.JwtUtils;
+import com.dggl.amei.configuration.security.services.UserDetailsImpl;
+import com.dggl.amei.dtos.requests.LoginRequest;
+import com.dggl.amei.dtos.requests.SignupRequest;
+import com.dggl.amei.dtos.responses.JwtResponse;
+import com.dggl.amei.dtos.responses.MessageResponse;
+import com.dggl.amei.enums.EnumRole;
+import com.dggl.amei.models.RefreshToken;
+import com.dggl.amei.models.Role;
+import com.dggl.amei.models.User;
+import com.dggl.amei.repositories.RoleRepository;
+import com.dggl.amei.repositories.UserRepository;
+import com.dggl.amei.services.RefreshTokenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+//@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    RoleRepository roleRepository;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Autowired
+    RefreshTokenService refreshTokenService;
+
+    @Autowired
+    JwtUtils jwtUtils;
+
+    @PostMapping("/signin")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword())
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = jwtUtils.generateJwtToken(authentication);
+
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+        List<String> roles = userDetails.getAuthorities().stream()
+                .map(item -> item.getAuthority())
+                .collect(Collectors.toList());
+
+        RefreshToken refreshToken = refreshTokenService.createRefreshToken(userDetails.getId());
+
+        return ResponseEntity.ok(new JwtResponse(jwt,
+                refreshToken.getToken(),
+                userDetails.getId(),
+                userDetails.getUsername(),
+                userDetails.getEmail(),
+                roles));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser (@Valid @RequestBody SignupRequest signupRequest) {
+        if (userRepository.existsByUsername(signupRequest.getUsername())) {
+            return ResponseEntity.badRequest()
+                    .body(new MessageResponse("Error: Username is already taken!"));
+        }
+
+        if (userRepository.existsByEmail(signupRequest.getEmail())) {
+            return ResponseEntity.badRequest()
+                    .body(new MessageResponse("Error: Email is already in use!"));
+        }
+
+        User user = new User(signupRequest.getUsername(), signupRequest.getEmail(),
+                passwordEncoder.encode(signupRequest.getPassword()));
+
+        Set<String> strRoles = signupRequest.getRole();
+        Set<Role> roles = new HashSet<>();
+
+        List<Role> all = roleRepository.findAll();
+
+        if (all.isEmpty()) {
+            Role role = new Role();
+            role.setName(EnumRole.ROLE_ADMIN);
+            roleRepository.save(role);
+            Role role2 = new Role();
+            role2.setName(EnumRole.ROLE_USER);
+            roleRepository.save(role2);
+        }
+
+        if (strRoles == null) {
+            Role userRole = roleRepository.findByName(EnumRole.ROLE_USER)
+                    .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+            roles.add(userRole);
+        } else {
+            strRoles.forEach(role -> {
+                switch (role) {
+                    case "admin":
+                        Role adminRole = roleRepository.findByName(EnumRole.ROLE_ADMIN)
+                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                        roles.add(adminRole);
+
+                        break;
+                    default:
+                        Role userRole = roleRepository.findByName(EnumRole.ROLE_USER)
+                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                        roles.add(userRole);
+                }
+            });
+        }
+
+        user.setRoles(roles);
+        userRepository.save(user);
+
+        return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
+    }
+}
--- src/main/java/com/dggl/amei/repositories/RoleRepository.java
+++ src/main/java/com/dggl/amei/repositories/RoleRepository.java
@@ -1,5 +1,6 @@
 package com.dggl.amei.repositories;
 
+import com.dggl.amei.enums.EnumRole;
 import com.dggl.amei.models.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,5 +9,5 @@ import java.util.Optional;
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
-    Optional<Role> findByName(String name);
+    Optional<Role> findByName(EnumRole name);
 }
