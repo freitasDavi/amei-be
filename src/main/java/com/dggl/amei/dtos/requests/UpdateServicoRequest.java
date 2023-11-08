48a4a445362e07c9b61dd7d3ca9c44fc549fd0c2
Davi @Senac <davi.frrs@gmail.com>
2023-11-08 19:26:05 -0300

    Adiconado dto faltante

---
+++ src/main/java/com/dggl/amei/dtos/requests/UpdateServicoRequest.java
@@ -0,0 +1,13 @@
+package com.dggl.amei.dtos.requests;
+
+public class UpdateServicoRequest extends NovoServicoRequest {
+    public Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
