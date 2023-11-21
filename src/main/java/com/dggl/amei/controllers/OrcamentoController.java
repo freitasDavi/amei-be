2ae9d06befb77a6fca86b1c720365bb796af3055
Guilherme Hahn <guilherme.f.h@hotmail.com>
2023-11-21 11:56:51 -0300

    Ajustado para queres nos JPA. Exportações levadas para seus respectivos services. Incluido exportação para agendamentos também. Removido o CsvExportService

--- src/main/java/com/dggl/amei/controllers/OrcamentoController.java
+++ src/main/java/com/dggl/amei/controllers/OrcamentoController.java
@@ -4,20 +4,15 @@ import com.dggl.amei.dtos.requests.NovoOrcamentoRequest;
 import com.dggl.amei.dtos.requests.PeriodoDTO;
 import com.dggl.amei.dtos.requests.UpdateOrcamentoRequest;
 import com.dggl.amei.models.Orcamento;
-import com.dggl.amei.services.CsvExportService;
 import com.dggl.amei.services.OrcamentoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.net.URI;
-import java.time.LocalDateTime;
-import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/orcamentos")
@@ -25,9 +20,6 @@ public class  OrcamentoController extends AbstractController {
 
     @Autowired
     private OrcamentoService service;
-
-    @Autowired
-    private CsvExportService csvExportService;
 
     @GetMapping
     public ResponseEntity<Page<Orcamento>> findAll(
