bda10f9820f83efbbc82effe30d5c12666644356
Davi Freitas da Silva <davi.frrs@gmail.com>
2023-12-11 10:44:57 -0300

    Update CorsConfiguration.java

--- src/main/java/com/dggl/amei/configuration/CorsConfiguration.java
+++ src/main/java/com/dggl/amei/configuration/CorsConfiguration.java
@@ -10,7 +10,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "https://amei.onrender.com/")
+                .allowedOrigins("http://localhost:5173", "https://amei.onrender.com/", "https://amei-pv6l.onrender.com/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
 }
